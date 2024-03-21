7e7d89f65eb4e63466178a16941c83dc7ca93e92
gtebbutt <5956226+gtebbutt@users.noreply.github.com>
2024-03-21 20:49:11 +0000

    Clean up dynamic scaling

--- ridge/models/embeddings.py
+++ ridge/models/embeddings.py
@@ -124,6 +124,8 @@ class MultiAxisRotaryPositionEmbed(nn.Module):
         embed_dim: int,
         axes: int = 2,
         max_trained_sequence_length: int,
+        interpolation_type: Optional[str] = None,
+        dynamic_interpolation: bool = False,
         training_interpolation_scale: float = 1.0,
         theta: float = 10000.0,
     ):
@@ -134,36 +136,78 @@ class MultiAxisRotaryPositionEmbed(nn.Module):
         self.axes = axes
         self.theta = theta
 
-        # The different types of scaling and when they get applied can sometimes be confusing; this one is a training-time variable used to more quickly fine tune and existing model to a larger (or smaller) target sequence length by mapping new positions into the range that the model already understands
+        # The different types of scaling and when they get applied can sometimes be confusing; this one is used at training time to more quickly fine tune an existing model to a larger (or smaller) target sequence length by mapping new positions into the range that the model already understands, and then remains fixed at that value to maintain the base scaling for inference with those weights
         # Good visualisation and writeup at https://blog.eleuther.ai/yarn/#position-interpolation
         self.training_interpolation_scale = training_interpolation_scale
 
-        # This is used to scale inference-time interpolation if applicable, and is independent of training_interpolation_scale. At training time it should be set to the maximum sequence length in the new training dataset, even if fine tuning from an existing checkpoint with a shorter trained sequence length
+        # This is used to more precisely scale embeddings for each individual sequence at inference time if dynamic_interpolation is enabled. At training time it should be set to the maximum sequence length in the new training dataset, even if fine tuning from an existing checkpoint with a shorter trained sequence length
         self.max_trained_sequence_length = max_trained_sequence_length
+
+        valid_interpolation_types = {None, "linear", "ntk", "yarn"}
+        if interpolation_type not in valid_interpolation_types:
+            raise ValueError(f"Invalid interpolation type: {interpolation_type}, expected one of {', '.join(valid_interpolation_types)}")
+
+        self.interpolation_type = interpolation_type
+        self.dynamic_interpolation = dynamic_interpolation
 
     # Linear implementations generally cache this in a buffer, but doing so for more axes triggers torch recompilation every time the shape changes, and adds potentially significant memory overhead unless you know up front you're only using a single aspect ratio
     def calculate_freqs(
         self,
         shape: List[int],
-        interpolation_type: Optional[str] = None,
+        # Functionally equivalent to alpha in the original dynamic NTK release, but also accounts for separate fine tuning with NTK scaling; values somewhere around 2.0 to 4.0 are generally reasonable
+        ntk_alpha: Optional[float] = None,
     ):
         if not len(shape) == self.axes:
             raise ValueError(f"Expected a {self.axes}-axis shape, but got {shape}")
 
+        if self.training:
+            if self.dynamic_interpolation:
+                raise RuntimeError(f"Dynamic interpolation should not be enabled during training: max_trained_sequence_length should be set equal to the longest sequence expected in the training dataset")
+            
+            if ntk_alpha is not None:
+                raise ValueError(f"ntk_alpha is an inference parameter and should not be set during training; training_interpolation_scale should be used as the alpha value for training - see comments in ridge.models.embeddings for further details")
+
+        
+        if ntk_alpha is None:
+            if self.dynamic_interpolation:
+                # 1.0 means no further scaling
+                ntk_alpha = 1.0
+        else:
+            if not self.dynamic_interpolation:
+                raise ValueError(f"ntk_alpha is only compatible with dynamic interpolation. To statically scale the whole embedding space (NTK-aware interpolation), use training_interpolation_scale as the alpha value")
+
         input_sequence_length = reduce(lambda a, b: a * b, shape)
 
-        if interpolation_type is None or input_sequence_length <= self.max_trained_sequence_length:
-            theta = self.theta
-        elif interpolation_type == "dynamic-ntk":
-            # This currently assumes uniform scaling across all axes, which gives greater overall flexibility and works well for real-world image generation use, but it can easily be extended to apply per-axis by tracking the max trained length for each axis rather than for the sequence as a whole and calculating a separate theta value for each; may be useful to do so if extrapolating to aspect ratios significantly beyond those in the training data
-            exponent = (self.embed_dim / (self.embed_dim - 2))
-            theta = self.theta * ((self.training_interpolation_scale * input_sequence_length / self.max_trained_sequence_length) - (self.training_interpolation_scale - 1)) ** exponent
+        if self.interpolation_type is None:
+            interpolation_scale = 1.0
         else:
-            raise ValueError(f"Invalid interpolation type: {interpolation_type}")
+            # This class currently assumes uniform scaling across all axes, which gives greater overall flexibility and works well for real-world image generation use, but it can easily be extended to apply per-axis by tracking the static interpolation scale (and/or max trained length) for each axis rather than for the sequence as a whole and calculating a separate interpolation scale for each; may be useful to do so if extrapolating to aspect ratios very significantly beyond those in the training data
+
+            # All interpolation types will, at minimum, apply a constant scaling factor (which may be 1.0 in the case of a model that hasn't been fine tuned above its original max sequence length)
+            interpolation_scale = self.training_interpolation_scale
+
+            # Dynamic scaling further increases the interpolation factor based on the input sequence length if it's longer than the trained maximum
+            if self.dynamic_interpolation and input_sequence_length > self.max_trained_sequence_length:
+                interpolation_scale = interpolation_scale * input_sequence_length / self.max_trained_sequence_length
+            else:
+                # Only apply the inference time NTK alpha scaling to longer sequences - this matches how the dynamic NTK formula is originally described, although the caching in the reference implementation overwrites the shorter sequence embeddings the first time a longer sequence is used, meaning the actual behaviour of that version can be inconsistent
+                # This will also ensure the correct calculation when dynamic mode is disabled, without breaking the input validation above
+                ntk_alpha = 1.0
+
+        if self.interpolation_type == "ntk":
+            exponent = (self.embed_dim / (self.embed_dim - 2))
+            # This slightly extends /u/emozilla's original dynamic NTK implementation by defining alpha=(self.training_interpolation_scale*ntk_alpha)
+            # It allows a static training alpha to be set for much faster fine tuning of pretrained models, which is then locked in as the baseline and scaled at inference time by ntk_alpha for better long-range performance
+            theta = self.theta * ((interpolation_scale * ntk_alpha) - ((self.training_interpolation_scale * ntk_alpha) - 1)) ** exponent
+        else:
+            theta = self.theta
 
         # One tensor for each axis, each containing the cartesian coordinates on that axis for every point in the space; meshgrid returns int64 coordinates by default
         axis_positions = torch.meshgrid([torch.arange(l) for l in shape], indexing="ij")
         axis_positions = [pos.to(torch.float32) * self.training_interpolation_scale for pos in axis_positions]
+
+        if self.interpolation_type == "linear":
+            axis_positions = axis_positions / interpolation_scale
 
         # Explicitly using fp32 here, since autocast is disabled in the forward function
         # Factor of two per axis to account for the fact we'll be repeating this for each axis, and then using the overall result twice to get the sin and cos components in the forward function
@@ -181,7 +225,6 @@ class MultiAxisRotaryPositionEmbed(nn.Module):
         query: torch.Tensor,
         key: torch.Tensor,
         shapes: List[List[int]],
-        interpolation_type: Optional[str] = None,
     ):
         if len(set(shapes)) != 1:
             raise NotImplementedError(f"Heterogeneous batches are not yet supported")
@@ -189,13 +232,12 @@ class MultiAxisRotaryPositionEmbed(nn.Module):
             shape = shapes[0]
 
         # Query and key shape are both (b n h d) where h is num_attention_heads and d is attention_head_dim (which should be equal to embed_dim); for DiT and Pixart models num_attention_heads=16 and attention_head_dim=72
-        batch_size = query.shape[0]
         sequence_length = query.shape[1]
         input_dtype = query.dtype
 
         # Always run these calculations in fp32, even if autocast is enabled - longer sequences get unstable if the sin and cos values are handled in reduced precision
         with torch.cuda.amp.autocast(enabled=False):
-            freqs = self.calculate_freqs(shape, interpolation_type) # (n d/2)
+            freqs = self.calculate_freqs(shape) # (n d/2)
 
             # Equivalent to torch.polar but using the last dimension for (real, imaginary) rather than using complex dtypes, as they aren't compatible with torch.compile
             freqs = torch.stack([freqs.cos(), freqs.sin()], dim=-1)
